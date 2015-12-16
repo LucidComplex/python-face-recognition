1a3de1c7457312f87fd70bf284e0029f801d7463
Paks <jnpakson@gmail.com>
2015-12-17 01:51:26 +0800

    Added Train tab UI

--- YourFaceSoundsFamiliar.py
+++ YourFaceSoundsFamiliar.py
@@ -5,19 +5,27 @@ from pyforms.Controls import ControlText, ControlLabel, ControlButton, ControlIm
 class YourFaceSoundsFamiliar(BaseWidget):
     def __init__(self):
         super(YourFaceSoundsFamiliar,self).__init__('Your Face Sounds Familiar')
+        #Predict Tab
         self._imagepath = ControlText('Path')
-        self._browsebutton = ControlButton('Browse')
+        self._browsebuttonpredict = ControlButton('Browse')
         self._predictimage = ControlImage()
         self._predictbutton = ControlButton('Predict')
         self._predicteddetails = ControlLabel('Details')
         self._name = ControlLabel('Name: ')
         self._fscore = ControlLabel('FScore: ')
+        #Train Tab
+        self._pername = ControlText('Name')
+        self._browsebuttontrain = ControlButton('Browse')
+        self._dirpath = ControlText('Path')
+        self._imagetotrain = ControlImage()
+        self._trainbutton = ControlButton('Train')
         self._formset = [ {
-            'Predict':['_imagepath','||','_browsebutton','=','_predictimage'
-                       ,'=','_predictbutton','=',
+            'Predict':['_imagepath','||','_browsebuttonpredict','=','_predictimage',
+                       '=','_predictbutton','=',
                        '_predicteddetails','=','_name',
                        '=','_fscore'],
-            'Train': ['']
+            'Train': ['_pername','=','_dirpath','||','_browsebuttontrain',
+                      '=','_imagetotrain','=','_trainbutton']
             } ]
 
 
