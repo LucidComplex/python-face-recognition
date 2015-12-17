b21a173a99e30d94476c41d71121f985f799f12b
Paks <jnpakson@gmail.com>
2015-12-17 15:04:56 +0800

    face detection using haarcascade

--- FaceDetection.py
+++ FaceDetection.py
@@ -4,14 +4,15 @@ import numpy as np
 
 class FaceDetection():
     def __init__(self):
-        self._cascPath = "haarcascade_frontalface_default.xml"
+        self._cascPath = "haarcascade_frontalface_alt.xml"
 
     def  __detect_face(self, image):
         faceCascade = cv2.CascadeClassifier(self._cascPath)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("fasdf",gray)
         faces = faceCascade.detectMultiScale(
                     gray,
-                    scaleFactor=1.2,
+                    scaleFactor=1.01,
                     minNeighbors=5,
                     minSize=(30, 30),
                     flags=cv2.cv.CV_HAAR_SCALE_IMAGE
@@ -19,6 +20,7 @@ class FaceDetection():
         return faces
 
     def cropface(self, image):
+        print(image)
         faces = self.__detect_face(image)
         for (x, y, w, h) in faces:
             croppedface = image[y:y+h,x:x+w]
@@ -30,6 +32,11 @@ class FaceDetection():
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
         return image
 
+    def resizeimage(self, image):
+        r = 90.0 / image.shape[1]
+        dim = (90, int(image.shape[0] * r))
+        resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+        return resized
 
 if __name__ == '__main__':
     FaceDetection('/home/paks/Pictures/bang.jpg')
