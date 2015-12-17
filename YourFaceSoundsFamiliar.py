1a55886f4b177999a1fb4bc3619b7f2004af1ecb
Paks <jnpakson@gmail.com>
2015-12-18 01:57:13 +0800

    needed features

--- YourFaceSoundsFamiliar.py
+++ YourFaceSoundsFamiliar.py
@@ -27,31 +27,35 @@ class YourFaceSoundsFamiliar(BaseWidget):
         self._selectdir.changed = self.__change_path_dir
         self._imagetotrain = ControlImage()
         self._trainbutton = ControlButton('Train')
-        self._formset = [ {
+        self._trainbutton.value = self.__trainbAction
+        self._formset = [{
             'Predict':['_selectfile','=','_predictimage',
                        '=','_predictbutton','=',
                        '_predicteddetails','=','_name',
                        '=','_fscore'],
             'Train': ['_pername','=','_selectdir',
                       '=','_imagetotrain','=','_trainbutton']
-            } ]
+            }]
 
 
     def __change_path(self):
-        image =  cv2.imread(self._selectfile.value)
+        image = cv2.imread(self._selectfile.value)
         self._predictimage.value = FaceDetection().drawrectangle(image)
 
 
     def __change_path_dir(self):
+        self._imagetotrain.value = []
         listofimages = os.listdir(self._selectdir.value)
         listofimages = [cv2.imread(os.path.join(self._selectdir.value, filename)) for filename in listofimages]
         resizedimages = [FaceDetection().resizeimageb(image) for image in listofimages]
         croppedimages = [FaceDetection().cropface(image) for image in resizedimages]
         resizedcroppedimages = [FaceDetection().resizeimagea(image)[0] for image in croppedimages]
         resizedcroppedimagesgray = [FaceDetection().resizeimagea(image)[1] for image in croppedimages]
-        trainingset = [np.array(image).flatten() for image in resizedcroppedimagesgray]
+        self.trainingset = [np.array(image).flatten() for image in resizedcroppedimagesgray]
         self._imagetotrain.value = resizedcroppedimages
 
+    def __trainbAction(self):
+        np.savetxt("trainingset_"+ self._pername.value+".csv",self.trainingset,delimiter=",")
 
 if __name__ == '__main__':
     pyforms.startApp(YourFaceSoundsFamiliar)
